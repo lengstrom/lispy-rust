926e482f2cf7fee590e7ed9fc8966c65e2b632a2
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-05-07 12:30:36 -0400

    added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+extern crate regex;
+
 fn main() {
-    println!("Hello, world!");
+	// Execute script from the command line
+}
+
+fn parse(code: &mut str) -> Vec<&str>{
+	// code.to_string()
+	let leftParenRe = Regex::new("\s*\(\s*").unwrap();
+	let rightParenRe = Regex::new("\s*\)\s*").unwrap();
+	let parsedCode = rightParenRe.replace_all(leftParenRe.replace_all(code, " ( ")," ) ")
+	parsedCode.split(" ")
+}
+
+fn tokenize() {
+
+}
+
+fn read_from_tokens() {
+
 }
 
 #[test]
 fn it_works() {
-
+	let parsedCode : Vec<&str> = parse("(begin (define r 10) (* pi (* r r)))");
+	println!("{}", parsedCode);
 }
