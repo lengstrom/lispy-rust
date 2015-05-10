0d5cf88c7425d3b36a771d9af144ed3a4ee71d0a
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-05-10 18:52:12 -0400

    added more

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn parse(code: &str) -> Vec<&str> {
 	// code.to_string()
 	let leftParenRe = regex::Regex::new(r"\s*\(\s*").unwrap();
 	let rightParenRe = regex::Regex::new(r"\s*\)\s*").unwrap();
-	let partialParsedCode = leftParenRe.replace_all(code, r" ( ");
-	let parsedCode = rightParenRe.replace_all(partialParsedCode.as_slice()," ) ");
+	let partialParsedCode = leftParenRe.replace_all(&code, " ( ");
+	let parsedCode = rightParenRe.replace_all(&partialParsedCode," ) ");
 	// let parsedCode = code.replace(" (", "(").replace("( ", "(").replace("(", " ( ").replace()
 	parsedCode.as_slice().split(" ").collect()
 }
@@ -24,6 +24,7 @@ fn read_from_tokens() {
 
 #[test]
 fn it_works() {
-	let parsedCode : Vec<&str> = parse("(begin (define r 10) (* pi (* r r)))");
+	let input = "(begin (define r 10) (* pi (* r r)))";
+	let parsedCode = parse(input);
 	println!("{}", parsedCode);
 }
