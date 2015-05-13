06ccd3e35dc863dd7dcf209a91fc4ab808002c75
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-05-12 23:49:03 -0400

    logging nightmare

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ fn main() {
 	println!("!!!!");
 	let input = "(begin (define r 10) (* pi (* r r)))";
 	let parsed_code = parse(input);
-	println!("{}", parsed_code);
+	println!("{}", arr2str(parsed_code));
 	// Execute script from the command line
 }
 
-fn parse(code: &str) -> String {
+fn arr2str(a:Vec<&str>) -> &str {
+	let mut s = "[";
+	for i in a.iter() {
+		s = concat!(s, i);
+	}
+	s = concat!(s, " ]");
+	s;
+}
+
+fn parse(code: &str) -> Vec<&str> {
 	// code.to_string()
 	let left_paren_re = regex::Regex::new(r"\s*\(\s*").unwrap();
 	let right_paren_re = regex::Regex::new(r"\s*\)\s*").unwrap();
 	let partial_parsed_code = left_paren_re.replace_all(&code, " ( ");
 	let parsed_code = right_paren_re.replace_all(&partial_parsed_code," ) ");
 	// let parsed_code = code.replace(" (", "(").replace("( ", "(").replace("(", " ( ").replace()
-	let final_string : String = parsed_code.split(" ").collect();
+	let final_string : Vec<&str> = parsed_code.split(" ").collect();
 	final_string.clone()
 }
 
